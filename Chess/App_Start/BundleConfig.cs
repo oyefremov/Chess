df6856c8033608ddc1c0ab9b387eccc3f9302177
Serhiy Hryn <shryn@opentext.com>
2014-12-06 11:32:11 +0200

    add jQuery.UI

--- Chess/App_Start/BundleConfig.cs
+++ Chess/App_Start/BundleConfig.cs
@@ -13,6 +13,9 @@ namespace Chess
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                 "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+            "~/Scripts/jquery-ui-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
                 "~/Scripts/knockout-{version}.js",
                 "~/Scripts/knockout.validation.js"));
@@ -42,6 +45,20 @@ namespace Chess
             bundles.Add(new StyleBundle("~/Content/css").Include(
                  "~/Content/bootstrap.css",
                  "~/Content/Site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+              "~/Content/themes/base/jquery.ui.core.css",
+              "~/Content/themes/base/jquery.ui.resizable.css",
+              "~/Content/themes/base/jquery.ui.selectable.css",
+              "~/Content/themes/base/jquery.ui.accordion.css",
+              "~/Content/themes/base/jquery.ui.autocomplete.css",
+              "~/Content/themes/base/jquery.ui.button.css",
+              "~/Content/themes/base/jquery.ui.dialog.css",
+              "~/Content/themes/base/jquery.ui.slider.css",
+              "~/Content/themes/base/jquery.ui.tabs.css",
+              "~/Content/themes/base/jquery.ui.datepicker.css",
+              "~/Content/themes/base/jquery.ui.progressbar.css",
+              "~/Content/themes/base/jquery.ui.theme.css"));
         }
     }
 }
